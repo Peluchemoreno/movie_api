064e9895895a6f3a53114f812e4edb3cecb08f93
Peluchemoreno <uhhh.quigley2@gmail.com>
2023-01-04 18:57:08 -0600

    if applied this should allow creations of servers within node using http, fs, and url modules

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
-const http = require("http");
-const url = require("url");
+const http = require("http"),
+  fs = require("fs"),
+  url = require("url");
 
 http
   .createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("hello node!\n");
+    let addr = request.url,
+      q = url.parse(addr, true),
+      filePath = "";
+
+    fs.appendFile(
+      "logs.txt",
+      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log.");
+        }
+      }
+    );
+
+    if (q.pathname.includes("documentation")) {
+      filePath = __dirname + "/documentation.html";
+    } else {
+      filePath = "index.html";
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(data);
+      response.end();
+    });
   })
   .listen(8080);
-
-url;
-
-console.log("this is my first node server and it is on port 8080");
+console.log("My test server is running on Port 8080.");
