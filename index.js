aaced2ea941d105e064da99224119dcaafce8c43
Peluchemoreno <uhhh.quigley2@gmail.com>
2023-01-21 22:24:41 -0600

    if applied this should give realtime data for my api

--- index.js
+++ index.js
@@ -33,15 +33,124 @@ let topMovies = [
       description: 'a film in which very frightening or unnatural things happen, for example dead people coming to life and people being murdered.'
     }
   },
-  { title: 'author1' },
-  { title: 'author2' },
-  { title: 'author3' },
-  { title: 'author4' },
-  { title: 'author5' },
-  { title: 'author6' },
-  { title: 'author7' },
-  { title: 'author8' },
-  { title: 'author9' },
+  {
+    title: 'Dragon Ball Z: Battle of Gods',
+    description: 'The Z-Fighters must contend with Lord Beerus, the God of Destruction, but only a God can fight a God, and none of them are Gods. However with the creation of the Super Saiyan God, will the Z-Fighters be able to defeat Lord Beerus?',
+    director: {
+      name: 'Masahiro Hosoda',
+      bio: 'a really cool director',
+      age: 44,
+    },
+    genre: {
+      name: 'Animation',
+      description: 'Animated Films are ones in which individual drawings, paintings, or illustrations are photographed frame by frame (stop-frame cinematography).'
+    }
+  },
+  {
+    title: 'Inside Out',
+    description: 'After young Riley is uprooted from her Midwest life and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school.',
+    director: {
+      name: 'Pete Docter',
+      bio: 'a really cool director',
+      age: 44,
+    },
+    genre: {
+      name: 'Animation',
+      description: 'Animated Films are ones in which individual drawings, paintings, or illustrations are photographed frame by frame (stop-frame cinematography).'
+    }
+  },
+  {
+    title: 'Jurassic Park',
+    description: "A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.",
+    director: {
+      name: 'Steven Spielberg',
+      bio: 'a really cool director',
+      age: 44,
+    },
+    genre: {
+      name: 'Sci-Fi',
+      description: 'a form of fiction that deals principally with the impact of actual or imagined science upon society or individuals'
+    }
+  },
+  {
+    title: 'Shutter Island',
+    description: 'In 1954, a U.S. Marshal investigates the disappearance of a murderer who escaped from a hospital for the criminally insane.',
+    director: {
+      name: 'Martin Scorsese',
+      bio: 'a really cool director',
+      age: 44,
+    },
+    genre: {
+      name: 'Mystery',
+      description: 'The mystery genre is a genre of fiction that follows a crime (like a murder or a disappearance) from the moment it is committed to the moment it is solved.'
+    }
+  },
+  {
+    title: 'Toy Story',
+    description: "A cowboy doll is profoundly threatened and jealous when a new spaceman action figure supplants him as top toy in a boy's bedroom.",
+    director: {
+      name: 'John Lasseter',
+      bio: 'a really cool director',
+      age: 74,
+    },
+    genre: {
+      name: 'Animation',
+      description: 'Animated Films are ones in which individual drawings, paintings, or illustrations are photographed frame by frame (stop-frame cinematography).'
+    }
+  },
+  {
+    title: 'Django Unchained',
+    description: 'With the help of a German bounty-hunter, a freed slave sets out to rescue his wife from a brutal plantation-owner in Mississippi.',
+    director: {
+      name: 'Quentin Tarantino',
+      bio: 'a really cool director',
+      age: 44,
+    },
+    genre: {
+      name: 'Drama',
+      description: 'drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
+    }
+  },
+  {
+    title: 'Spirited Away',
+    description: "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.",
+    director: {
+      name: 'Hayao Miyazaki',
+      bio: 'a really cool director',
+      age: 50,
+    },
+    genre: {
+      name: 'Animation',
+      description: 'Animated Films are ones in which individual drawings, paintings, or illustrations are photographed frame by frame (stop-frame cinematography).'
+    }
+  },
+  {
+    title: 'Inception',
+    description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
+    director: {
+      name: 'Christopher Nolan',
+      bio: 'a really cool director',
+      age: 40,
+    },
+    genre: {
+      name: 'Sci-Fi',
+      description: 'a form of fiction that deals principally with the impact of actual or imagined science upon society or individuals'
+    }
+  },
+  {
+    title: 'The Shawshank Redemption',
+    description: 'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.',
+    director: {
+      name: 'Frank Darabont',
+      bio: 'a really cool director',
+      age: 50,
+    },
+    genre: {
+      name: 'Drama',
+      description: 'drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
+    }
+  },
+
 ]
 
 //middleware functions
